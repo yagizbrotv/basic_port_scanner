39785d18afe9ec1af7dc63d8e502fe79fa2cd27e
Yağız Sarıdoğan <72974768+yagizbrotv@users.noreply.github.com>
2023-11-14 15:15:56 +0300

    Update : service running on the port added basicportscanner.py
    Port 80 is open. ++Service: http
    Port 443 is open. ++Service: https

--- basicportscanner.py
+++ basicportscanner.py
@@ -1,4 +1,12 @@
 import socket
+import sys
+
+def get_service_name(port):
+    try:
+        service_name = socket.getservbyport(port)
+        return service_name
+    except (socket.error, OSError):
+        return "Unknown Service"
 
 def scan_ports(target, start_port, end_port):
     print(f"Scanning target {target} for open ports...\n")
@@ -9,13 +17,14 @@ def scan_ports(target, start_port, end_port):
             sock.settimeout(1)
             result = sock.connect_ex((target, port))
             if result == 0:
-                count = count + 1
-                print(f"Port {port} is open")
+                count += 1
+                service_name = get_service_name(port)
+                print(f"Port {port} is open. Service: {service_name}")
             sock.close()
     except KeyboardInterrupt:
         print("\nScan interrupted by user. Exiting...")
         sys.exit(1)
-    print(f"There are {count} ports open out of {end_port-start_port+1} ports")
+    print(f"There are {count} ports open out of {end_port - start_port + 1} ports")
 
 if __name__ == "__main__":
     target_host = input("Enter the target IP address: ")
@@ -23,4 +32,3 @@ if __name__ == "__main__":
     end_port = int(input("Enter the ending port: "))
     
     scan_ports(target_host, start_port, end_port)
-
