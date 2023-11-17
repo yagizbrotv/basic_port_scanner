b3e3b217ede6c99f9254d398ed1b0f50f0950607
Yağız Sarıdoğan <72974768+yagizbrotv@users.noreply.github.com>
2023-11-17 22:53:02 +0300

    Bash update basicportscanner.py
    Takes inputs as bash

--- basicportscanner.py
+++ basicportscanner.py
@@ -27,8 +27,14 @@ def scan_ports(target, start_port, end_port):
     print(f"There are {count} ports open out of {end_port - start_port + 1} ports")
 
 if __name__ == "__main__":
-    target_host = input("Enter the target IP address: ")
-    start_port = int(input("Enter the starting port: "))
-    end_port = int(input("Enter the ending port: "))
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <target_ip> <start_port> <end_port>")
+        sys.exit(1)
+
+    target_host = sys.argv[1]
+    start_port = int(sys.argv[2])
+    end_port = int(sys.argv[3])
     
     scan_ports(target_host, start_port, end_port)
+
